213ae5a94ddf37c6e39e5031ec7052d488d68a2d
ABHIJEET <abhijeetsahoomsd@gmail.com>
2024-01-05 00:20:26 +0530

    added profileimages to the system

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const {ServerConfig, Connect} = require("./config");
 const apiRoutes = require("./routes/index");
 
-
-
 const app = express();
 
 app.use(express.json());
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,7 +3,7 @@ const {signUp, logIn} = require("../../controllers/auth-controller");
 const { postImage } = require("../../controllers/image-controller");
 const { upload } = require("../../services/upload");
 
-const router = express.Router();
+const   router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/login", logIn);
